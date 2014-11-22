a8be0c533943d56bb3dd7b7a26a685f20358a11d
dgreentheawesome <dgreen8789@aol.com>
2014-11-21 21:12:25 -0600

    Click targeting added, amended uncaught nullpointer exception

--- src/input/MouseRunner.java
+++ src/input/MouseRunner.java
@@ -5,6 +5,8 @@
  */
 package input;
 
+import game.Board;
+import game.Locations;
 import game.Main;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -17,11 +19,36 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class MouseRunner implements MouseListener {
 
     private AtomicInteger width;
-    private String initialLoc;
-    private String endLoc;
+    private String initialLoc = "";
+    private String endLoc = "";
+    private Locations[] clicked;
 
     @Override
+
     public void mouseClicked(MouseEvent e) {
+        clicked[0] = clicked[1];
+        int[] squareData = Main.getGui().getGraphicsControl().getSquare(e.getLocationOnScreen());
+        if (squareData != null){
+        String squareString = new String(Character.toChars(squareData[1])).concat(Integer.toString(squareData[0]));
+        System.out.println(squareString);
+        int x1 = (squareString.charAt(0)) - 97;
+        int y1 = Integer.parseInt(squareString.substring(1)) - 1;
+        clicked[1] = new Locations(x1, y1);
+        if (clicked[0] != null && clicked[1] != null) {
+            if (Main.getGame().validPos(clicked[0]) == 0) {
+                Board board = Main.getGame().getBoard();
+                if (board.movePiece(clicked[0], clicked[1])) {
+                    board.printBoard();
+                    Main.getGui().getGraphicsControl().updateBoard(board);
+                    Main.getGame().newTurn();
+                }
+            }
+        }
+        }
+    }
+
+    public MouseRunner() {
+        clicked = new Locations[2];
     }
 
     @Override
@@ -30,6 +57,7 @@ public class MouseRunner implements MouseListener {
         if (squareData != null) {
             initialLoc = (new String(Character.toChars(squareData[1])).concat(Integer.toString(squareData[0])));
         }
+
     }
 
     @Override
